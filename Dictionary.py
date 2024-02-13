de1d4849a5602b0c8bec9afe8d817cbe4bc2627b
hasann321 <rakibhasan00point@gmail.com>
2024-02-13 22:12:42 +0600

    dictionary commented

--- Dictionary.py
+++ Dictionary.py
@@ -16,5 +16,5 @@ myDict = {"you": "Yes its you", "my": "belongs to me", "dict": "beloved dictiona
 # for value in myDict.values():
 #     print(value)
 
-for x, y in myDict.items():
-    print(x, "=>", y)
+# for x, y in myDict.items():
+#     print(x, "=>", y)
